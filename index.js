d4a0ce7bf1a5ae1dc33b2cb615ab4ca57937d169
GeoReece000 <geofreytegeret@gmail.com>
2023-03-31 14:29:51 +0300

    Resubmit

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+beforeAll(async (done) => {
+    // do some asynchronous setup
+    await asyncFunction();
+    done();
+  }, 10000); // set a 10 second timeout
